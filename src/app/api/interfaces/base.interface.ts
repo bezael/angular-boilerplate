ebedd90e022c1df038ff0c347712d04e8921efae
Bezael <bezael@gmail.com>
2022-02-01 20:39:00 +0100

    Add Service and Interface

---
+++ src/app/api/interfaces/base.interface.ts
@@ -0,0 +1,22 @@
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface RequestOptions {
+  headers?: HttpHeaders | { [header: string]: string | string[] },
+  observe?: 'body' | 'events' | 'response',
+  params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> },
+  reportProgress?: boolean,
+  responseType?: 'json',
+  withCredentials?: boolean,
+}
+
+
+export interface BaseInterface {
+  get<T>(endPoint: string, options?: RequestOptions): Observable<T>;
+
+  post<T>(endPoint: string, params: Object, options?: RequestOptions): Observable<T>;
+
+  put<T>(endPoint: string, params: Object, options?: RequestOptions): Observable<T>;
+
+  delete<T>(endPoint: string, options?: RequestOptions): Observable<T>;
+}
