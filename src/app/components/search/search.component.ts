ab85c6989233090fe0a94dac8da6d51ad415a398
CosmeValera <cosmevalerareales@gmail.com>
2022-07-08 15:08:00 +0200

    add style to brand and search elements

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
+  template: `
+    <div class="search">
+      <input class="search__input" placeholder="City..." [formControl]="inputSearch"/>
+    </div>
+  `,
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  inputSearch = new FormControl('');
+  @Output() submitted = new EventEmitter<string>();
 
   ngOnInit(): void {
+    this.onChange();
+  }
 
+  private onChange(): void {
+    this.inputSearch.valueChanges
+      .pipe(
+        map( (search: string) => search.trim()),
+        debounceTime(850),
+        distinctUntilChanged(),
+        filter( (search: string) => search !== ''),
+        tap( (search: string) => console.log(search))
+      )
+      .subscribe();
   }
 
 }
--- src/app/components/search/search.module.ts
+++ src/app/components/search/search.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { SearchComponent } from './search.component';
     SearchComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [SearchComponent]
 })
