434b8999782231e3b7b08d22c5e334c7a9b6ab60
Bezael <bezael@gmail.com>
2022-12-12 19:39:05 +0100

    feat(base): add new method get by id

--- src/app/api/interfaces/base.interface.ts
+++ src/app/api/interfaces/base.interface.ts
@@ -4,19 +4,24 @@ import { Observable } from 'rxjs';
 export interface RequestOptions {
   headers?: HttpHeaders | { [header: string]: string | string[] },
   observe?: 'body',
-  params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> },
+  params?:
+    | HttpParams
+    | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>
+      },
   reportProgress?: boolean,
   responseType?: 'json',
   withCredentials?: boolean,
 }
 
-
 export interface BaseInterface {
+  getById<T>(endPoint: string, options?: RequestOptions): Observable<T>;
   get<T>(endPoint: string, options?: RequestOptions): Observable<T>;
-
-  post<T>(endPoint: string, options?: RequestOptions): Observable<T>;
-
-  put<T>(endPoint: string, options?: RequestOptions): Observable<T>;
-
   delete<T>(endPoint: string, options?: RequestOptions): Observable<T>;
+  post<T>(endPoint: string, options?: RequestOptions): Observable<T>;
+  put<T>(endPoint: string, options?: RequestOptions): Observable<T>;
 }
