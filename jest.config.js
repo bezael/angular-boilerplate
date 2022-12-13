62e4381c5fe4761275fa9d1d07c59e0ec8bf6229
Bezael <bezael@gmail.com>
2022-12-13 11:21:30 +0100

    fix(test): fix module not found

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    '@env/(.*)': '<rootDir>/src/environments/$1',
+  },
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+    globalSetup: 'jest-preset-angular/global-setup',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
