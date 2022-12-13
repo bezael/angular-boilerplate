b46dced8f8c445b693761d363f6dff17eb38b237
Dany Paredes <danywalls4@gmail.com>
2022-12-13 08:56:07 +0100

    Update base.service.spec.ts
    Cover response and request type

--- src/app/api/services/base.service.spec.ts
+++ src/app/api/services/base.service.spec.ts
@@ -1,28 +1,62 @@
-import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {BaseService} from './base.service';
+import { HttpClient } from '@angular/common/http';
 
-interface MockAPI {
+interface ApiResponseModel {
   id: string;
   status: boolean;
   requestedDate: Date;
 }
 
+
+const mockAPIResponse : Array<ApiResponseModel> = [
+  {
+    id: Math.random().toString(),
+    requestedDate: new Date(),
+    status: true
+  }
+]
+
+const API_FAKE_RESOURCE = 'entity'
+const API_FAKE_HOST_RESOURCE = '/entity'
 describe('base service', () => {
   let service: BaseService;
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
 
-  TestBed.configureTestingModule(
-    {
-      providers: [ BaseService],
-      imports: [ HttpClientModule]
-    }
-  )
   beforeEach(() => {
+
+    TestBed.configureTestingModule(
+      {
+        providers: [ BaseService],
+        imports: [ HttpClientTestingModule]
+      }
+    )
+
+    httpClient = TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(BaseService)
   })
 
-  it('get return observable of type', () => {
-    let response = service.get<MockAPI>('fakeAPI');
-    expect(response).not.toBe(null)
+  afterEach(() => {
+    httpTestingController.verify()
+  })
+
+  it('get fake response', () => {service.get<ApiResponseModel>(API_FAKE_RESOURCE).subscribe((response) => {
+      expect(response).toEqual(mockAPIResponse)
+    })
+    const httpRequest = httpTestingController.expectOne(API_FAKE_HOST_RESOURCE)
+    expect(httpRequest.request.method).toEqual('GET')
+
+  })
+
+  it('post ', () => {
+    const fakePost = mockAPIResponse[0];
+    service.post<ApiResponseModel>(API_FAKE_RESOURCE).subscribe((response: ApiResponseModel) => {
+      expect(response).toEqual(fakePost)
+    })
+    const httpRequest = httpTestingController.expectOne(API_FAKE_HOST_RESOURCE);
+    expect(httpRequest.request.method).toEqual('POST')
   })
 })
